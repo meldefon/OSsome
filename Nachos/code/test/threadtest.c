223e481015a60438581c446112fd376b89d8448f
Gabriel Mel <meldefon@usc.edu>
2015-11-27 01:03:00 -0800

    FINALLY. Part 1 appears to be done. Had to clean up many things. This included: line size, some name issues, initialization duplication, CV creation where it should have been a lock, name length issues, and more that I can't remember now. More checking is in order, but running the sim with inputs 1 of each clerk, + 2 custs + 0 sens runs and finishes.

--- Nachos/code/test/threadtest.c
+++ Nachos/code/test/threadtest.c
@@ -34,29 +34,29 @@ void Uprintf(char *string, int length, int num_1, int num_2, int num_3, int num_
 void createServerMVs(int numCustomers, int numberOfSenators) {
 	int i;
 	/*global shared data between the clerks that are used for filing purposes */
-	numCustomersLeft = CreateMV("numCustomersLeft", 17, 1);
-	bribesEnabled = CreateMV("bribesEnabled", 14, 1);
+	numCustomersLeft = CreateMV("numCustomersLeft", 16, 1);
+	bribesEnabled = CreateMV("bribesEnabled", 13, 1);
 
-	customersWithCompletedApps = CreateMV("customersWithCompletedApps", 27, numCustomers);
-	customersWithCompletedPics = CreateMV("customersWithCompletedPics", 27, numCustomers);
-	passportClerkChecked = CreateMV("passportClerkChecked", 21, numCustomers);
-	cashierChecked = CreateMV("cashierChecked", 15, numCustomers);
-	gottenPassport = CreateMV("gottenPassport", 15, numCustomers);
-	cashReceived = CreateMV("cashReceived", 13, numCustomers);
+	customersWithCompletedApps = CreateMV("customersWithCompletedApps", 26, numCustomers);
+	customersWithCompletedPics = CreateMV("customersWithCompletedPics", 26, numCustomers);
+	passportClerkChecked = CreateMV("passportClerkChecked", 20, numCustomers);
+	cashierChecked = CreateMV("cashierChecked", 14, numCustomers);
+	gottenPassport = CreateMV("gottenPassport", 14, numCustomers);
+	cashReceived = CreateMV("cashReceived", 12, numCustomers);
 
-	appClerkCurrentCustomer = CreateMV("appClerkCurrentCustomer", 24, numCustomers);
-	pictureClerkCurrentCustomer = CreateMV("pictureClerkCurrentCustomer", 28, numCustomers);
-	passportClerkCurrentCustomer = CreateMV("passportClerkCurrentCustomer", 29, numCustomers);
-	cashierCurrentCustomer = CreateMV("cashierCurrentCustomer", 23, numCustomers);
+	appClerkCurrentCustomer = CreateMV("appClerkCurrentCustomer", 23, numCustomers);
+	pictureClerkCurrentCustomer = CreateMV("pictureClerkCurrentCustomer", 27, numCustomers);
+	passportClerkCurrentCustomer = CreateMV("passportClerkCurrentCustomer", 28, numCustomers);
+	cashierCurrentCustomer = CreateMV("cashierCurrentCustomer", 22, numCustomers);
 
-	senatorLock = CreateLock("senatorLock", 12);
-	senatorCV = CreateCondition("senatorCV", 10);
-	isSenator = CreateMV("isSenator", 10, numCustomers);
-	senatorWorking = CreateMV("senatorWorking", 15, 1);
-	clerksCanWork = CreateMV("clerksCanWork", 14, 1);
+	senatorLock = CreateLock("senatorLock", 11);
+	senatorCV = CreateCondition("senatorCV", 9);
+	isSenator = CreateMV("isSenator", 9, numCustomers);
+	senatorWorking = CreateMV("senatorWorking", 14, 1);
+	clerksCanWork = CreateMV("clerksCanWork", 13, 1);
 
-	newCustomerId = CreateMV("newCustomerId", 14, 1);
-	newCustomerIdLock = CreateLock("newCustomerIdLock", 18);
+	newCustomerId = CreateMV("newCustomerId", 13, 1);
+	newCustomerIdLock = CreateLock("newCustomerIdLock", 17);
 
 	/*Initialize everything*/
 		
@@ -112,89 +112,89 @@ void initialize(struct Monitor *m, int clerkType_, int size) {
 	nameLength = 50;
 
 	if(clerkType_ == 0) {
-		m->lineLock = CreateLock("lineLock0", 10);
-		m->newClerkIdLock = CreateLock("newClerkIdLock0", 16);					
-		m->newClerkId = CreateMV("newClerkId0", 12, 1);
-		m->clerkType = CreateMV("clerkType0", 11, 1);
-		m->lineCV = CreateMV("lineCV0", 8, size);
-		m->bribeLineCV = CreateMV("bribeLineCV0", 13, size);
-		m->senLineCV = CreateMV("senLineCV0", 11, size);
-		m->limboLineCV = CreateMV("limboLineCV0", 13, size);
-		m->clerkLock = CreateMV("clerkLock0", 11, size);
-		m->clerkCV = CreateMV("clerkCV0", 9, size);
-		m->breakCV = CreateCondition("breakCV0", 9);
-		m->lineCount = CreateMV("lineCount0", 11, size);
-		m->bribeLineCount = CreateMV("bribeLineCount0", 16, size);
-		m->senLineCount = CreateMV("senLineCount0", 14, size);
-		m->clerkState = CreateMV("clerkState0", 12, size);
-		m->numOfClerks = CreateMV("numOfClerks0", 13, 1);
-		m->cashReceived = CreateMV("cashReceived0", 14, 1);
+		m->lineLock = CreateLock("lineLock0", 9);
+		m->newClerkIdLock = CreateLock("newClerkIdLock0", 15);
+		m->newClerkId = CreateMV("newClerkId0", 11, 1);
+		m->clerkType = CreateMV("clerkType0", 10, 1);
+		m->lineCV = CreateMV("lineCV0", 7, size);
+		m->bribeLineCV = CreateMV("bribeLineCV0", 12, size);
+		m->senLineCV = CreateMV("senLineCV0", 10, size);
+		m->limboLineCV = CreateMV("limboLineCV0", 12, size);
+		m->clerkLock = CreateMV("clerkLock0", 10, size);
+		m->clerkCV = CreateMV("clerkCV0", 8, size);
+		m->breakCV = CreateCondition("breakCV0", 8);
+		m->lineCount = CreateMV("lineCount0", 10, size);
+		m->bribeLineCount = CreateMV("bribeLineCount0", 15, size);
+		m->senLineCount = CreateMV("senLineCount0", 13, size);
+		m->clerkState = CreateMV("clerkState0", 11, size);
+		m->numOfClerks = CreateMV("numOfClerks0", 12, 1);
+		m->cashReceived = CreateMV("cashReceived0", 13, 1);
 		m->currentCustomer = CreateMV("currentCustomer0", 17, size);
-		m->numCustomersInLimbo = CreateMV("numCustomersInLimbo0", 21, 1);
-		lockName = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\0";
+		m->numCustomersInLimbo = CreateMV("numCustomersInLimbo0", 20, 1);
+		lockName = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 	} else if(clerkType_ == 1) {
-		m->lineLock = CreateLock("lineLock1", 10);
-		m->newClerkIdLock = CreateLock("newClerkIdLock1", 16);											
-		m->newClerkId = CreateMV("newClerkId1", 12, 1);
-		m->clerkType = CreateMV("clerkType1", 11, 1);
-		m->lineCV = CreateMV("lineCV1", 8, size);
-		m->bribeLineCV = CreateMV("bribeLineCV1", 13, size);
-		m->senLineCV = CreateMV("senLineCV1", 11, size);
-		m->limboLineCV = CreateMV("limboLineCV0", 13, size);
-		m->clerkLock = CreateMV("clerkLock1", 11, size);
-		m->clerkCV = CreateMV("clerkCV1", 9, size);
-		m->breakCV = CreateCondition("breakCV1", 9);
-		m->lineCount = CreateMV("lineCount1", 11, size);
-		m->bribeLineCount = CreateMV("bribeLineCount1", 16, size);
-		m->senLineCount = CreateMV("senLineCount1", 14, size);
-		m->clerkState = CreateMV("clerkState1", 12, size);
-		m->numOfClerks = CreateMV("numOfClerks1", 13, 1);
-		m->cashReceived = CreateMV("cashReceived1", 14, 1);
-		m->currentCustomer = CreateMV("currentCustomer1", 17, size);
-		m->numCustomersInLimbo = CreateMV("numCustomersInLimbo1", 21, 1);
-		lockName = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB\0";
+		m->lineLock = CreateLock("lineLock1", 9);
+		m->newClerkIdLock = CreateLock("newClerkIdLock1", 15);
+		m->newClerkId = CreateMV("newClerkId1", 11, 1);
+		m->clerkType = CreateMV("clerkType1", 10, 1);
+		m->lineCV = CreateMV("lineCV1", 7, size);
+		m->bribeLineCV = CreateMV("bribeLineCV1", 12, size);
+		m->senLineCV = CreateMV("senLineCV1", 10, size);
+		m->limboLineCV = CreateMV("limboLineCV0", 12, size);
+		m->clerkLock = CreateMV("clerkLock1", 10, size);
+		m->clerkCV = CreateMV("clerkCV1", 8, size);
+		m->breakCV = CreateCondition("breakCV1", 8);
+		m->lineCount = CreateMV("lineCount1", 10, size);
+		m->bribeLineCount = CreateMV("bribeLineCount1", 15, size);
+		m->senLineCount = CreateMV("senLineCount1", 13, size);
+		m->clerkState = CreateMV("clerkState1", 11, size);
+		m->numOfClerks = CreateMV("numOfClerks1", 12, 1);
+		m->cashReceived = CreateMV("cashReceived1", 13, 1);
+		m->currentCustomer = CreateMV("currentCustomer1", 16, size);
+		m->numCustomersInLimbo = CreateMV("numCustomersInLimbo1", 20, 1);
+		lockName = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
 	} else if(clerkType_ == 2) {
-		m->lineLock = CreateLock("lineLock2", 10);
-		m->newClerkIdLock = CreateLock("newClerkIdLock2", 16);											
-		m->newClerkId = CreateMV("newClerkId2", 12, 1);
-		m->clerkType = CreateMV("clerkType2", 11, 1);
-		m->lineCV = CreateMV("lineCV2", 8, size);
-		m->bribeLineCV = CreateMV("bribeLineCV2", 13, size);
-		m->senLineCV = CreateMV("senLineCV2", 11, size);
-		m->limboLineCV = CreateMV("limboLineCV2", 13, size);
-		m->clerkLock = CreateMV("clerkLock2", 11, size);
-		m->clerkCV = CreateMV("clerkCV2", 9, size);
-		m->breakCV = CreateCondition("breakCV2", 9);
-		m->lineCount = CreateMV("lineCount2", 11, size);
-		m->bribeLineCount = CreateMV("bribeLineCount2", 16, size);
-		m->senLineCount = CreateMV("senLineCount2", 14, size);
-		m->clerkState = CreateMV("clerkState2", 12, size);
-		m->numOfClerks = CreateMV("numOfClerks2", 13, 1);
-		m->cashReceived = CreateMV("cashReceived2", 14, 1);
-		m->currentCustomer = CreateMV("currentCustomer2", 17, size);
-		m->numCustomersInLimbo = CreateMV("numCustomersInLimbo2", 21, 1);
-		lockName = "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC\0";		
+		m->lineLock = CreateLock("lineLock2", 9);
+		m->newClerkIdLock = CreateLock("newClerkIdLock2", 15);
+		m->newClerkId = CreateMV("newClerkId2", 11, 1);
+		m->clerkType = CreateMV("clerkType2", 10, 1);
+		m->lineCV = CreateMV("lineCV2", 7, size);
+		m->bribeLineCV = CreateMV("bribeLineCV2", 12, size);
+		m->senLineCV = CreateMV("senLineCV2", 10, size);
+		m->limboLineCV = CreateMV("limboLineCV2", 12, size);
+		m->clerkLock = CreateMV("clerkLock2", 10, size);
+		m->clerkCV = CreateMV("clerkCV2", 8, size);
+		m->breakCV = CreateCondition("breakCV2", 8);
+		m->lineCount = CreateMV("lineCount2", 10, size);
+		m->bribeLineCount = CreateMV("bribeLineCount2", 15, size);
+		m->senLineCount = CreateMV("senLineCount2", 13, size);
+		m->clerkState = CreateMV("clerkState2", 11, size);
+		m->numOfClerks = CreateMV("numOfClerks2", 12, 1);
+		m->cashReceived = CreateMV("cashReceived2", 13, 1);
+		m->currentCustomer = CreateMV("currentCustomer2", 16, size);
+		m->numCustomersInLimbo = CreateMV("numCustomersInLimbo2", 20, 1);
+		lockName = "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC";
 	} else if(clerkType_ == 3) {
-		m->lineLock = CreateLock("lineLock3", 10);
-		m->newClerkIdLock = CreateLock("newClerkIdLock3", 16);											
-		m->newClerkId = CreateMV("newClerkId3", 12, 1);
-		m->clerkType = CreateMV("clerkType3", 11, 1);
-		m->lineCV = CreateMV("lineCV3", 8, size);
-		m->bribeLineCV = CreateMV("bribeLineCV3", 13, size);
-		m->senLineCV = CreateMV("senLineCV3", 11, size);
-		m->limboLineCV = CreateMV("limboLineCV3", 13, size);
-		m->clerkLock = CreateMV("clerkLock3", 11, size);
-		m->clerkCV = CreateMV("clerkCV3", 9, size);
-		m->breakCV = CreateCondition("breakCV3", 9);
-		m->lineCount = CreateMV("lineCount3", 11, size);
-		m->bribeLineCount = CreateMV("bribeLineCount3", 16, size);
-		m->senLineCount = CreateMV("senLineCount3", 14, size);
-		m->clerkState = CreateMV("clerkState3", 12, size);
-		m->numOfClerks = CreateMV("numOfClerks3", 13, 1);
-		m->cashReceived = CreateMV("cashReceived3", 14, 1);
-		m->currentCustomer = CreateMV("currentCustomer3", 17, size);
-		m->numCustomersInLimbo = CreateMV("numCustomersInLimbo3", 21, 1);
-		lockName = "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD\0";
+		m->lineLock = CreateLock("lineLock3", 9);
+		m->newClerkIdLock = CreateLock("newClerkIdLock3", 15);
+		m->newClerkId = CreateMV("newClerkId3", 11, 1);
+		m->clerkType = CreateMV("clerkType3", 10, 1);
+		m->lineCV = CreateMV("lineCV3", 7, size);
+		m->bribeLineCV = CreateMV("bribeLineCV3", 12, size);
+		m->senLineCV = CreateMV("senLineCV3", 10, size);
+		m->limboLineCV = CreateMV("limboLineCV3", 12, size);
+		m->clerkLock = CreateMV("clerkLock3", 10, size);
+		m->clerkCV = CreateMV("clerkCV3", 8, size);
+		m->breakCV = CreateCondition("breakCV3", 8);
+		m->lineCount = CreateMV("lineCount3", 10, size);
+		m->bribeLineCount = CreateMV("bribeLineCount3", 15, size);
+		m->senLineCount = CreateMV("senLineCount3", 13, size);
+		m->clerkState = CreateMV("clerkState3", 11, size);
+		m->numOfClerks = CreateMV("numOfClerks3", 12, 1);
+		m->cashReceived = CreateMV("cashReceived3", 13, 1);
+		m->currentCustomer = CreateMV("currentCustomer3", 16, size);
+		m->numCustomersInLimbo = CreateMV("numCustomersInLimbo3", 20, 1);
+		lockName = "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD";
 	}
 	
 	SetMV(m->newClerkId, 0, 0);
@@ -213,7 +213,7 @@ void initialize(struct Monitor *m, int clerkType_, int size) {
 		*/
 
 		SetMV(m->lineCV, i, CreateCondition(lockName, nameLength));
-		SetMV(m->clerkLock, i, CreateCondition(lockName, nameLength));
+		SetMV(m->clerkLock, i, CreateLock(lockName, nameLength));
 		nameLength--;
 		SetMV(m->clerkCV, i, CreateCondition(lockName, nameLength));
 		nameLength--;
@@ -293,6 +293,7 @@ int main() {
 		numberOfSenators = Scanf();
 		
 		size += numberOfSenators;
+		/*SetMV(numCustomersLeft, 0, size);*/
 
 		/* Create global server MVs and initialize everything */
 		createServerMVs(size, numberOfSenators);
